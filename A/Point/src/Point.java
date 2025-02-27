817beb40e878446b199b35a1e2ea36fec08fc6e3
Larry Clement <lclement@calbaptist.edu>
2025-02-27 10:26:41 -0800

    After section A on 2/27/2025

--- A/Point/src/Point.java
+++ A/Point/src/Point.java
@@ -1,5 +1,25 @@
+import java.awt.*;
+
 // TO DO:  Implement a class of our own named Point4
 public class Point {
     int x;
     int y;
+
+    public Point() {
+        this(0,0);
+    }
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public String toString() {
+        return "(" + x + ", " + y + ")";
+    }
+
+    public void draw(Graphics g) {
+        g.fillOval(x, y, 3, 3);
+        g.drawString("(" + x + ", " + y + ")", x+3, y-3);
+    }
 }
--- A/Point/src/PointClient.java
+++ A/Point/src/PointClient.java
@@ -7,8 +7,6 @@ public class PointClient {
         Point p1 = new Point();
 //        Point p2 = new Point(20, 100);
 
-        p1.x = 1;
-
         System.out.println("Point 1 is " + p1);
 //        System.out.println("Point 2 is " + p2);
 
@@ -20,14 +18,28 @@ public class PointClient {
 //        int sum = p2.x + p2.y;
 //        System.out.println("Sum of coordinates of p2 = " + sum);
 
-        p1.x = 5;
-        System.out.println(p1);
+        p1.y = 2;
+        p1.x = 4;
+        System.out.println(p1.x + ", " + p1.y);
 
-//        DrawingPanel panel = new DrawingPanel(300, 300);
-//        Graphics g = panel.getGraphics();
+        DrawingPanel panel = new DrawingPanel(300, 300);
+        Graphics g = panel.getGraphics();
+
+        p1.x = 15;
+        p1.y = 37;
+
+//        g.fillOval(p1.x, p1.y, 3, 3);
+//        g.drawString("(" + p1.x + ", " + p1.y + ")", p1.x+3, p1.y-3);
+
+        Point p2 = new Point(51, 79);
+//        p2.x = 51;
+//        p2.y = 79;
+
+//        g.fillOval(p2.x, p2.y, 3, 3);
+//        g.drawString("(" + p2.x + ", " + p2.y + ")", p2.x+3, p2.y-3);
 
         //The Java version of the Point class does not know how to draw itself - we will have to implement our own to do this
-//        p1.draw(g);
-//        p2.draw(g);
+        p1.draw(g);
+        p2.draw(g);
     }
 }
